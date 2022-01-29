1066c1e461ce094fbc42d0602f56fa0852fe3032
Layssa <layssa.lima166@gmail.com>
2022-01-29 02:02:42 -0300

    completed delete

--- src/controllers/delete.js
+++ src/controllers/delete.js
@@ -2,14 +2,13 @@ const { Book } = require("../models/book");
 
 const DeleteBook = async (req, res) => {
   const { id } = req.params;
-  console.log(id);
 
   if (!id) return res.status(200).send({ status: "400", msg: "Invalid id" });
 
   try {
     const deletedBook = await Book.deleteOne({ id: id });
     res.status(200).send({ data: "deleted book!" });
-    
+
   } catch (error) {
     console.log(error);
     res.status(200).send({ status: 500, msg: "deleted book" });
